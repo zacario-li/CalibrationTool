9163c305987c2329652793248b65e6844c54b184
lizhijian <lzjcyy@qq.com>
2023-08-28 22:29:41 +0800

    delete unused menu bar

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -10,28 +10,6 @@ class MainWindow(wx.Frame):
         self.SetMinSize(self.GetSize())
         self.SetMaxSize(self.GetSize())
 
-        # Create menu bar
-        self.menu_bar = wx.MenuBar()
-
-        file_menu = wx.Menu()
-        new_item = file_menu.Append(wx.ID_NEW, 'New')
-        open_item = file_menu.Append(wx.ID_OPEN, 'Open')
-        save_item = file_menu.Append(wx.ID_SAVE, 'Save')
-        exit_item = file_menu.Append(wx.ID_EXIT, 'Exit')
-
-        edit_menu = wx.Menu()
-        cut_item = edit_menu.Append(wx.ID_CUT, 'Cut')
-        copy_item = edit_menu.Append(wx.ID_COPY, 'Copy')
-        paste_item = edit_menu.Append(wx.ID_PASTE, 'Paste')
-
-        help_menu = wx.Menu()
-        about_item = help_menu.Append(wx.ID_ABOUT, 'About')
-
-        self.menu_bar.Append(file_menu, 'File')
-        self.menu_bar.Append(edit_menu, 'Edit')
-        self.menu_bar.Append(help_menu, 'Help')
-        self.SetMenuBar(self.menu_bar)
-
         # Create notebook with 3 tabs
         self.notebook = wx.Notebook(self)
 
