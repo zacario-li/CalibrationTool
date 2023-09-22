b19e493a44f6ed50d021835246a6df6bab8c78cd
lizhijian <lizj@microport.com>
2023-09-22 13:40:44 +0800

    reformat code

--- ui/tabHandEye.py
+++ ui/tabHandEye.py
@@ -13,115 +13,136 @@ class TabHandEye():
         self.tab.SetSizer(self.m_layout_he_main)
         self.create_main_ui()
 
-    def create_main_ui(self):
+    def _create_he_type(self):
         m_layout_he_type = wx.BoxSizer(wx.HORIZONTAL)
 
-        m_radioBox1Choices = [u"AX=XB", u"AX=YB"]
-        self.m_radioBox1 = wx.RadioBox(self.tab, wx.ID_ANY, u"Calib Type", wx.DefaultPosition,
-                                       wx.DefaultSize, m_radioBox1Choices, 1, wx.RA_SPECIFY_ROWS)
-        self.m_radioBox1.SetSelection(0)
-        m_layout_he_type.Add(self.m_radioBox1, 0,
+        m_radioBoxChoices_calib_type = [u"AX=XB", u"AX=YB"]
+        self.m_radioBox_calib_type = wx.RadioBox(self.tab, wx.ID_ANY, u"Calib Type", wx.DefaultPosition,
+                                       wx.DefaultSize, m_radioBoxChoices_calib_type, 1, wx.RA_SPECIFY_ROWS)
+        self.m_radioBox_calib_type.SetSelection(0)
+        m_layout_he_type.Add(self.m_radioBox_calib_type, 0,
                              wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
 
-        self.m_checkBox1 = wx.CheckBox(
+        self.m_checkBox_rotation_only = wx.CheckBox(
             self.tab, wx.ID_ANY, u"R Only", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_type.Add(self.m_checkBox1, 0,
-                             wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+        m_layout_he_type.Add(self.m_checkBox_rotation_only, 0,
+                             wx.ALIGN_CENTER_VERTICAL | wx.ALL, 0)
+        return m_layout_he_type
 
-        self.m_layout_he_main.Add(m_layout_he_type, 1, wx.EXPAND, 5)
-
+    def _create_he_calib_method(self):
         m_layout_he_calib_method = wx.BoxSizer(wx.HORIZONTAL)
 
-        m_radioBox2Choices = [u"TSAI", u"PARK",
+        m_radioBoxChoices_axxb_calib_method = [u"TSAI", u"PARK",
                               u"HORAUD", u"ANDREFF", u"DANIILIDIS"]
-        self.m_radioBox2 = wx.RadioBox(self.tab, wx.ID_ANY, u"AX=XB Calib Method",
-                                       wx.DefaultPosition, wx.DefaultSize, m_radioBox2Choices, 1, wx.RA_SPECIFY_ROWS)
-        self.m_radioBox2.SetSelection(2)
-        m_layout_he_calib_method.Add(self.m_radioBox2, 0, wx.ALL, 5)
+        self.m_radioBox_axxb_calib_method = wx.RadioBox(self.tab, wx.ID_ANY, u"AX=XB Calib Method",
+                                       wx.DefaultPosition, wx.DefaultSize, m_radioBoxChoices_axxb_calib_method, 1, wx.RA_SPECIFY_ROWS)
+        self.m_radioBox_axxb_calib_method.SetSelection(2)
+        m_layout_he_calib_method.Add(self.m_radioBox_axxb_calib_method, 0, wx.ALL, 5)
 
-        m_radioBox3Choices = [u"SHAH", u"LI"]
-        self.m_radioBox3 = wx.RadioBox(self.tab, wx.ID_ANY, u"AX=YB Calib Method",
-                                       wx.DefaultPosition, wx.DefaultSize, m_radioBox3Choices, 1, wx.RA_SPECIFY_ROWS)
-        self.m_radioBox3.SetSelection(0)
-        self.m_radioBox3.Enable(False)
+        m_radioBoxChoices_axyb_calib_method = [u"SHAH", u"LI"]
+        self.m_radioBox_axyb_calib_method = wx.RadioBox(self.tab, wx.ID_ANY, u"AX=YB Calib Method",
+                                       wx.DefaultPosition, wx.DefaultSize, m_radioBoxChoices_axyb_calib_method, 1, wx.RA_SPECIFY_ROWS)
+        self.m_radioBox_axyb_calib_method.SetSelection(0)
+        self.m_radioBox_axyb_calib_method.Enable(False)
 
-        m_layout_he_calib_method.Add(self.m_radioBox3, 0, wx.ALL, 5)
+        m_layout_he_calib_method.Add(self.m_radioBox_axyb_calib_method, 0, wx.ALL, 5)
+        return m_layout_he_calib_method
 
-        self.m_layout_he_main.Add(m_layout_he_calib_method, 2, wx.EXPAND, 5)
-
-        m_layout__he_dataloader = wx.BoxSizer(wx.HORIZONTAL)
+    def _create_he_dataloader(self):
+        m_layout_he_dataloader = wx.BoxSizer(wx.HORIZONTAL)
 
         m_layout_he_dataloader_path_main = wx.BoxSizer(wx.VERTICAL)
 
         m_layout_he_dataloader_A = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.m_textCtrl8 = wx.TextCtrl(
+        self.m_textCtrl_load_a_path = wx.TextCtrl(
             self.tab, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_A.Add(self.m_textCtrl8, 10, wx.ALL, 5)
-        self.m_textCtrl8.Enable(False)
+        m_layout_he_dataloader_A.Add(self.m_textCtrl_load_a_path, 10, wx.ALL, 1)
+        self.m_textCtrl_load_a_path.Enable(False)
 
-        self.m_button8 = wx.Button(
+        self.m_btn_loadA = wx.Button(
             self.tab, wx.ID_ANY, u"Load A", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_A.Add(self.m_button8, 0, wx.ALL, 5)
+        m_layout_he_dataloader_A.Add(self.m_btn_loadA, 0, wx.ALL, 1)
 
         m_layout_he_dataloader_path_main.Add(
-            m_layout_he_dataloader_A, 10, wx.EXPAND, 5)
+            m_layout_he_dataloader_A, 10, wx.EXPAND, 0)
 
-        m_layout_he_dataloader_BY = wx.BoxSizer(wx.HORIZONTAL)
+        m_layout_he_dataloader_B = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.m_textCtrl9 = wx.TextCtrl(
+        self.m_textCtrl_load_b_path = wx.TextCtrl(
             self.tab, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_BY.Add(self.m_textCtrl9, 10, wx.ALL, 5)
-        self.m_textCtrl9.Enable(False)
+        m_layout_he_dataloader_B.Add(self.m_textCtrl_load_b_path, 10, wx.ALL, 1)
+        self.m_textCtrl_load_b_path.Enable(False)
 
-        self.m_button9 = wx.Button(
+        self.m_btn_loadB = wx.Button(
             self.tab, wx.ID_ANY, u"Load B", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_BY.Add(self.m_button9, 0, wx.ALL, 5)
+        m_layout_he_dataloader_B.Add(self.m_btn_loadB, 0, wx.ALL, 1)
 
         m_layout_he_dataloader_path_main.Add(
-            m_layout_he_dataloader_BY, 10, wx.EXPAND, 5)
+            m_layout_he_dataloader_B, 10, wx.EXPAND, 0)
 
         m_layout_he_dataloader_param = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.m_textCtrl10 = wx.TextCtrl(
+        self.m_textCtrl_cam_param_path = wx.TextCtrl(
             self.tab, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_param.Add(self.m_textCtrl10, 10, wx.ALL, 5)
-        self.m_textCtrl10.Enable(False)
+        m_layout_he_dataloader_param.Add(self.m_textCtrl_cam_param_path, 10, wx.ALL, 1)
+        self.m_textCtrl_cam_param_path.Enable(False)
 
-        self.m_button10 = wx.Button(
+        self.m_btn_load_cam_param = wx.Button(
             self.tab, wx.ID_ANY, u"Load Camera Params", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_dataloader_param.Add(self.m_button10, 0, wx.ALL, 5)
+        m_layout_he_dataloader_param.Add(self.m_btn_load_cam_param, 0, wx.ALL, 1)
 
         m_layout_he_dataloader_path_main.Add(
-            m_layout_he_dataloader_param, 10, wx.EXPAND, 5)
+            m_layout_he_dataloader_param, 10, wx.EXPAND, 0)
 
-        m_layout__he_dataloader.Add(
-            m_layout_he_dataloader_path_main, 10, wx.EXPAND, 5)
+        m_layout_he_dataloader.Add(
+            m_layout_he_dataloader_path_main, 10, wx.EXPAND, 0)
 
         self.m_btn_calib = wx.Button(
             self.tab, wx.ID_ANY, u"Calib", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout__he_dataloader.Add(self.m_btn_calib, 0, wx.ALL | wx.EXPAND, 5)
+        m_layout_he_dataloader.Add(self.m_btn_calib, 0, wx.ALL | wx.EXPAND, 1)
 
         self.m_btn_save = wx.Button(
             self.tab, wx.ID_ANY, u"Save", wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout__he_dataloader.Add(self.m_btn_save, 1, wx.ALL | wx.EXPAND, 5)
+        m_layout_he_dataloader.Add(self.m_btn_save, 1, wx.ALL | wx.EXPAND, 1)
+        return m_layout_he_dataloader
 
-        self.m_layout_he_main.Add(m_layout__he_dataloader, 3, wx.EXPAND, 5)
-
+    def _create_he_view(self):
         m_layout_he_view = wx.BoxSizer(wx.VERTICAL)
 
-        self.m_bitmap3 = wx.StaticBitmap(
+        self.m_bitmap_checkview = wx.StaticBitmap(
             self.tab, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0)
-        m_layout_he_view.Add(self.m_bitmap3, 0, wx.ALL, 5)
+        m_layout_he_view.Add(self.m_bitmap_checkview, 0, wx.ALL, 5)
 
         self.m_statictext_calib_err_result = wx.StaticText(
             self.tab, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0)
         self.m_statictext_calib_err_result.Wrap(-1)
 
         m_layout_he_view.Add(self.m_statictext_calib_err_result, 0, wx.ALL, 5)
+        return m_layout_he_view
 
-        self.m_layout_he_main.Add(m_layout_he_view, 10, wx.EXPAND, 5)
-        pass
+    def create_main_ui(self):
+        # 手眼标定类型选择区域
+        m_layout_he_type = self._create_he_type()
+        self.m_layout_he_main.Add(m_layout_he_type, 1, wx.EXPAND, 0)
+
+        # 手眼标定方法选择区域
+        m_layout_he_calib_method = self._create_he_calib_method()
+        self.m_layout_he_main.Add(m_layout_he_calib_method, 2, wx.EXPAND, 0)
+
+        # 手眼标定数据选择区域
+        self.m_staticline_dataloader_upper = wx.StaticLine( self.tab, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        self.m_layout_he_main.Add( self.m_staticline_dataloader_upper, 0, wx.EXPAND |wx.ALL, 0 )
+        
+        m_layout_he_dataloader = self._create_he_dataloader()
+        self.m_layout_he_main.Add(m_layout_he_dataloader, 2, wx.EXPAND, 0)
+
+        self.m_staticline_dataloader_lower = wx.StaticLine( self.tab, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+        self.m_layout_he_main.Add( self.m_staticline_dataloader_lower, 0, wx.EXPAND |wx.ALL, 0 )
+
+        # 手眼标定结果查看区域
+        m_layout_he_view = self._create_he_view()        
+        self.m_layout_he_main.Add(m_layout_he_view, 20, wx.EXPAND, 0)
 
     def init_db(self):
         # db init
